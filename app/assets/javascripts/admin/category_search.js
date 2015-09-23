fd8238b8c116f24f63e6fddf4732bcfe97f6a19f
Bibek Sharma <bipashant@gmail.com>
2015-09-23 17:07:17 +0545

    Design issues fixed on the mobile view

--- app/assets/javascripts/admin/category_search.js
+++ app/assets/javascripts/admin/category_search.js
@@ -6,7 +6,18 @@
 (function () {
     $(document).ready(function () {
 
-        $('#category_list').chosen({});
+        $('#category_list').chosen({ width: '100%' });
+        $('#category_list1').chosen({ width: '100%' });
+
+        $('#category_list').on('change', function(e) {
+            $('#category_list1').val(this.value).trigger("chosen:updated");
+
+        });
+
+        $('#category_list1').on('change', function(e) {
+            $('#category_list').val(this.value).trigger("chosen:updated");
+
+        });
 
     });
 
